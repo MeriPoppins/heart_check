37c775a4ecafd13ab4650d1c1ed5af457c57070b
Мария <eva_mp@mail.ru>
2024-03-25 15:27:10 +0300

    Add files via upload
    add files

---
+++ my_app.py
@@ -0,0 +1,2 @@
+# напиши здесь код основного приложения и первого экрана
+C:\Program Files\Algoritmika\vscode\data\extensions\algoritmika.algopython-20231012.151548.0\data\student\3538911\test_rufie_2035_base
